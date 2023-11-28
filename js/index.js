6ea45e7cc7b7cab1bd2a7700a366aea3f08f9841
pcpo8992 <nash2665@gmail.com>
2023-11-29 07:51:27 +0800

    add block

--- js/index.js
+++ js/index.js
@@ -27,34 +27,65 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
+        // 监听第二个输入框的输入事件
+        document.getElementById("myInputPrice").addEventListener('input', function() {
+          // 将焦点设置回第一个输入框
+          
+          console.log("hello ");  
+      });
+
 // Create a new list item when clicking on the "Add" button
 function addPrice() {
+  var ul = document.querySelector("#myUL");
   var li = document.createElement("li");
   var inputValuePeople = document.getElementById("myInputPeople").value ;
   var inputValuePrice = document.getElementById("myInputPrice").value;
-  var finialValue = inputValuePeople +  "號民眾 喊價 " + inputValuePrice + " 元";
+  var finialValue = inputValuePeople +  "號競買人 喊價 " + inputValuePrice + " 元";
   var t = document.createTextNode(finialValue);
+  // li.appendChild(t);
   li.appendChild(t);
+  
   if (inputValuePeople === '' || inputValuePrice === "") {
-    alert("必須輸入資料");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInputPeople").value = "";
-  document.getElementById("myInputPrice").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    if(inputValuePeople === ''){
+      alert("必須輸入競買人資料");
+      document.getElementById("myInputPeople").focus();
+    }else if(inputValuePrice === ""){
+      alert("必須輸入喊價金額資料");
+      document.getElementById("myInputPrice").focus();
     }
+  } else {
+    // document.getElementById("myUL").appendChild(li);
+    document.querySelector(".nowPrice").textContent = inputValuePrice + '元';
+
+    document.querySelector(".price").style.display = "block";
+
+    document.getElementById("myUL").prepend(li);
+    document.getElementById("myInputPeople").value = "";
+    document.getElementById("myInputPrice").value = "";
+  
+    document.getElementById("myInputPeople").focus();
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+
+    //add now price 
+    
   }
+
+  // li.prepend(span);
+
+
+
+
 }
 
 // 按 Enter 新增
