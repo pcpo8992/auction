5f7f9feaa26a3a4486a5ea16a98d33616e328778
pcpo8992 <nash2665@gmail.com>
2023-12-05 01:06:42 +0800

    add new item

--- js/index.js
+++ js/index.js
@@ -5,27 +5,23 @@ createDate();
 
 window.onload = function() {
     let i = 0;
-    // setInterval(function(){
-    //     i += 1;
-    //     document.querySelector(".nowPrice").textContent = i;
-    // }, 1000);
+
     createData();
-    setInterval(function(){
+
+    setInterval(function() {
         createDate();
+        // createData();
+        endAuction();
     }, 1000);
-    
+
 };
 
-function createDate(){
+function createDate() {
     let time = new Date();
     let finialTime = '<span style="font-size: 1em">(現在時間：' + time.toLocaleString() + ') </span>';
     document.querySelector("h3").innerHTML = finialTime;
 }
 
-    // setInterval(function(){
-    //   createData();
-    //   console.log(1);
-    // }, 1000);
 
 function numberComma(num) {
     let comma = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g
@@ -38,71 +34,15 @@ function numberComma(num) {
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 
-async function removeTag() {
-    // 
-    // if(document.querySelectorAll("li").length!= 0){
-    // //   for (let i = 0; i < document.querySelectorAll("li").length; i++) {
-    // //     document.querySelectorAll("li")[i].remove();
-
-    // //     console.log(document.querySelectorAll("li")[i]);
-    // // }
-    //  document.querySelectorAll("li")[0].remove()
-    // }
-    // return 'done';
-
-    while (true) {
-        if (document.querySelectorAll("li").length == 0) {
-            return 'done';
-            break;
-        } else {
-            document.querySelectorAll("li")[0].remove();
-        }
-    }
-}
-
-async function main(itemArray) {
-    const result = await removeTag();
-    // console.log(result);
-    // console.log(result);
-    for (let i = 0; i < itemArray.length; i++) {
-        if (itemArray[i].status == 1) {
-            // console.log(itemArray[i].itemID)
-            if (itemArray[i].price_detail != null) {
-                priceDetailArray = Object.values(itemArray[i].price_detail.li);
-            }
-        }
-    }
-    let ul = document.querySelector("#myUL");
-
-    if (priceDetailArray != null) {
-        for (let i = 0; i < priceDetailArray.length; i++) {
-            if (priceDetailArray[i]['myInputPeople'] != null) {
-                let li = document.createElement("li");
-                // var finialValue = inputValuePeople + "號競買人 喊價 " + numberComma(inputValuePrice) + " 元";
-                let finialValue = priceDetailArray[i]['myInputPeople'] + "號競買人 喊價 " + numberComma(priceDetailArray[i]['myInputPrice']).toString() + " 元";
-                // console.log(finialValue);
-                let t = document.createTextNode(finialValue);
-                // li.appendChild(t);
-                li.appendChild(t);
-                ul.prepend(li);
-                nowPrice.textContent = numberComma(priceDetailArray[i]['myInputPrice']) ;
-            }
-        }
-    }
-}
 
 function createData() {
-
-
     let firebaseConfig = {
-        // ...
         // The value of `databaseURL` depends on the location of the database
         databaseURL: "https://auction-88ffa-default-rtdb.firebaseio.com/",
     };
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
 
     // Initialize Realtime Database and get a reference to the service
     let database = firebase.database();
@@ -111,100 +51,153 @@ function createData() {
         item = e.val();
         if (item != null) {
             let itemArray = Object.values(item);
-            // console.log(document.querySelectorAll("li").length);
-
-            // removeTag();
-            // main(itemArray);
-            // for (let i = 0; i < document.querySelectorAll("li").length; i++) {
-            //     document.querySelectorAll("li")[i].remove();
-            //     console.log(i);
-            // }
-
-
-            // for (let i = 0; i < itemArray.length; i++) {
-            //     if (itemArray[i].status == 1) {
-            //         // console.log(itemArray[i].itemID)
-            //         if(itemArray[i].price_detail != null){
-            //           priceDetailArray = Object.values(itemArray[i].price_detail.li);
-            //         }
-            //     }
-            // }
-            // let ul = document.querySelector("#myUL");
-
-            // if(priceDetailArray != null){
-            //   for (let i = 0; i < priceDetailArray.length; i++) {
-            //     if (priceDetailArray[i]['myInputPeople'] != null) {
-            //         let li = document.createElement("li");
-            //         // var finialValue = inputValuePeople + "號競買人 喊價 " + numberComma(inputValuePrice) + " 元";
-            //         let finialValue = priceDetailArray[i]['myInputPeople'] + "號競買人 喊價 " + numberComma(priceDetailArray[i]['myInputPrice']).toString() + " 元";
-            //         // console.log(finialValue);
-            //         let t = document.createTextNode(finialValue);
-            //         // li.appendChild(t);
-            //         li.appendChild(t);
-            //         ul.prepend(li);
-            //         nowPrice.textContent = numberComma(priceDetailArray[i]['myInputPrice']) + " 元";
-            //     }
-            // }
-            // }
+            console.log(itemArray)
+            // document.querySelector("h2").textContent = "拍賣物名稱 : " + itemArray[i].itemName;
 
             while (true) {
                 if (document.querySelectorAll("li").length == 0) {
-                    // return 'done';
+                    document.querySelector('.nowPrice').textContent = '';
                     break;
                 } else {
                     document.querySelectorAll("li")[0].remove();
                 }
             }
+
+            priceDetailArray = null;
             for (let i = 0; i < itemArray.length; i++) {
-              if (itemArray[i].status == 1) {
-                  // console.log(itemArray[i].itemID)
-                  if (itemArray[i].price_detail != null) {
-                      let webId = itemArray[i].itemID;
-                      let webName = itemArray[i].itemName;
-                      console.log(webName);
-                    console.log(webId);
-                      document.querySelector("h2").textContent = "拍賣物名稱 : " + webName ;
-                      priceDetailArray = Object.values(itemArray[i].price_detail.li);
-                  }
-              }
-          }
-          let ul = document.querySelector("#myUL");
-      
-          if (priceDetailArray != null) {
-              for (let i = 0; i < priceDetailArray.length; i++) {
-                  if (priceDetailArray[i]['myInputPeople'] != null) {
-                      let li = document.createElement("li");
-                      // var finialValue = inputValuePeople + "號競買人 喊價 " + numberComma(inputValuePrice) + " 元";
-                      let finialValue = priceDetailArray[i]['myInputPeople'] + "號競買人 喊價 " + numberComma(priceDetailArray[i]['myInputPrice']).toString() + " 元";
-                      // console.log(finialValue);
-                      let t = document.createTextNode(finialValue);
-                      // li.appendChild(t);
-                      li.appendChild(t);
-                      ul.prepend(li);
-                      nowPrice.textContent = numberComma(priceDetailArray[i]['myInputPrice']) ;
-                    //   console.log(priceDetailArray.length);
-                    //   console.log(priceDetailArray);
-                  }else{
-                    // nowPrice.textContent = "";
-                  }
-              }
+                if (itemArray[i].status == 1 && itemArray.endAuction == null) {
+                    // console.log(itemArray[i].itemID)
+                    if (itemArray[i].itemLeastPrice != "") {
+                        document.querySelector("h2").innerHTML = "拍賣物名稱 : " + itemArray[i].itemName + "<br>最低加價金額：" + numberComma(itemArray[i].itemLeastPrice);
+                    } else {
+                        document.querySelector("h2").innerHTML = "拍賣物名稱 : " + itemArray[i].itemName + "<br>最低加價金額：" + itemArray[i].itemLeastPrice;
+                    }
 
-              if(priceDetailArray.length != 0 && priceDetailArray[0]['myInputPeople'] != null){
-                document.querySelector("li").setAttribute("class", "check");
-              }
-
-
-              let number = 0;
-              for (let i = 0; i < priceDetailArray.length; i++) {
-                if (priceDetailArray[i]['myInputPeople'] == null){
-                    number++;
+                    if (itemArray[i].price_detail != null) {
+                        let webId = itemArray[i].itemID;
+                        let webName = itemArray[i].itemName;
+                        // console.log(webName);
+                        // console.log(webId);
+                        // document.querySelector("h2").textContent = "拍賣物名稱 : " + webName;
+                        priceDetailArray = Object.values(itemArray[i].price_detail.li);
+                        // priceDetailArray = Object.values(itemArray[i].price_detail.li);
+                    }
                 }
-              }
-              if(priceDetailArray.length == number){
-                nowPrice.textContent = "";
-              }
+            }
+            let ul = document.querySelector("#myUL");
+            console.log(priceDetailArray);
 
-          }
+            if (priceDetailArray != null) {
+                for (let i = 0; i < priceDetailArray.length; i++) {
+                    if (priceDetailArray[i]['myInputPeople'] != null) {
+                        let li = document.createElement("li");
+                        // var finialValue = inputValuePeople + "號競買人 喊價 " + numberComma(inputValuePrice) + " 元";
+                        let finialValue = priceDetailArray[i]['myInputPeople'] + "號競買人 喊價 " + numberComma(priceDetailArray[i]['myInputPrice']).toString() + " 元";
+                        // console.log(finialValue);
+                        let t = document.createTextNode(finialValue);
+                        // li.appendChild(t);
+                        li.appendChild(t);
+                        ul.prepend(li);
+                        nowPrice.textContent = numberComma(priceDetailArray[i]['myInputPrice']);
+                        //   console.log(priceDetailArray.length);
+                        //   console.log(priceDetailArray);
+                    } else {
+                        // nowPrice.textContent = "";
+                    }
+                }
+
+                if (priceDetailArray.length != 0 && priceDetailArray[0]['myInputPeople'] != null) {
+                    document.querySelector("li").setAttribute("class", "check");
+                }
+
+
+                let number = 0;
+                for (let i = 0; i < priceDetailArray.length; i++) {
+                    if (priceDetailArray[i]['myInputPeople'] == null) {
+                        number++;
+                    }
+                }
+                if (priceDetailArray.length == number) {
+                    nowPrice.textContent = "";
+                }
+            }
         }
+    })
+}
+
+function endAuction() {
+    let firebaseConfig = {
+        // The value of `databaseURL` depends on the location of the database
+        databaseURL: "https://auction-88ffa-default-rtdb.firebaseio.com/",
+    };
+
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        firebase.app(); // if already initialized, use that one
+    }
+    // firebase.initializeApp();
+
+    // Initialize Realtime Database and get a reference to the service
+    let database = firebase.database();
+
+    database.ref("/").once('value', e => {
+        item = e.val();
+        if (item != null) {
+            let itemArray = Object.values(item);
+
+            console.log(itemArray.length);
+            let numberEndAuction = 0;
+            let numberEndAuction2 = 0;
+            // for (let i = 0; i < itemArray.length; i++) {
+            //     if (itemArray[i].endAuction == 1) {
+            //         numberEndAuction += 1;
+            //     } else if (itemArray[i].status == 2) {
+            //         numberEndAuction2 += 1;
+            //     }
+            // }
+            // console.log("number" + numberEndAuction);
+            // if (numberEndAuction == itemArray.length) {
+            if (itemArray[itemArray.length - 1].endAuction == 1) {
+                console.log("endAuction");
+                document.querySelector('h2').textContent = '拍賣競買人喊價';
+                document.querySelector('.nowPrice').textContent = '';
+
+                while (true) {
+                    if (document.querySelectorAll("li").length == 0) {
+                        break;
+                    } else {
+                        document.querySelectorAll("li")[0].remove();
+                    }
+                }
+            } else if (itemArray[itemArray.length - 1].status == 2) {
+                document.querySelector('h2').textContent = '拍賣競買人喊價';
+                document.querySelector('.nowPrice').textContent = '無人應買';
+                console.log("status == 2");
+                while (true) {
+                    if (document.querySelectorAll("li").length == 0) {
+                        break;
+                    } else {
+                        document.querySelectorAll("li")[0].remove();
+                    }
+                }
+            } else if (itemArray[itemArray.length - 1].status == 1) {
+                console.log("status == 1");
+                // document.querySelector('h2').textContent = '';
+                // document.querySelector('.nowPrice').textContent = '';
+
+                // while (true) {
+                //     if (document.querySelectorAll("li").length == 0) {
+                //         break;
+                //     } else {
+                //         document.querySelectorAll("li")[0].remove();
+                //     }
+            }
+        }else{
+            document.querySelector('h2').textContent = '拍賣競買人喊價';
+            console.log("無資料");
+        }
+        //   let ul = document.querySelector("#myUL");
     })
 }
